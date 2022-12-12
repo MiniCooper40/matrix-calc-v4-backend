6dfc75a03e03bc61fd0afb9fcd4b9403032ff303
ratbo <mcdavidfan97@gmail.com>
2022-12-11 17:33:15 -0800

    Initial commit

---
+++ src/test/java/com/matrixcalcv4backend/matrixcalcv4backend/MatrixCalcV4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrixcalcv4backend.matrixcalcv4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixCalcV4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
